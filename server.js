b733f72a5ae6dce8021bb9f693cf87cf45dca86b
tcolas673 <tcolas673@gmail.com>
2020-01-19 15:23:30 -0500

    server running on port 5000

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 // importing my http library to create server for communication
 const http = require('http');
-
+const express = require('express');
 // setting up my port that app will run on
 const PORT = 5000;
 
 // Setup a handler or a file that will hold my handlers
 const TempRouter = require('./tempData/tempRouter');
 
+//?
+const app = express();
+
 // attach handler to endpoint
-app.use('/', TempRouter);
+// app.use('/', TempRouter);
 
 const server = http.createServer(app);
 
